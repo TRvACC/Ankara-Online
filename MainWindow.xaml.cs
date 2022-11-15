2123f41bc3902fc732a49ea518a62e3882e2431f
cptalpdeniz <cptalpdeniz@gmail.com>
2022-11-15 00:47:29 -0800

    NavigationPane initial push

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,10 +17,9 @@ namespace Ankara_Online
         {
             this.InitializeComponent();
         }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            //left empty for now since I'll probably use this to manipulate or overload NavigateTo() function
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Ankara_Online
+This program is being developed to be used by ATCOs of VATSIM Türkiye, TRvACC. Some of the main features are:
+1. Download and install programs used by TRvACC
+2. Download and install current sector files and plugins
+3. Auto-updater for this program
+4. (FOR FUTURE) Event Calendar
+
+Developed by Alp Deniz Senyurt - 1230936
